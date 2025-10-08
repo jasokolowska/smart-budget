0e49fbf2e140240a43b4ac122f379f0595db2e6d
Joanna Sokolowska <joanna_s@op.pl>
2025-10-08 21:12:10 +0200

    checking pre-commit hook

--- server/app/src/main/java/com/sokolowska/app/AppApplication.java
+++ server/app/src/main/java/com/sokolowska/app/AppApplication.java
@@ -5,5 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.sokolowska.app", "com.sokolowska.transactions"})
 public class AppApplication {
-                public static void main(String[] args) {SpringApplication.run(AppApplication.class, args);}
+
+
+    public static void main(String[] args) {SpringApplication.run(AppApplication.class, args);}
 }
