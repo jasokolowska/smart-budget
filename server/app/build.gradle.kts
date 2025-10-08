7c57707a59763f2632cefbb096757e189508472e
Joanna Sokolowska <joanna_s@op.pl>
2025-10-08 21:22:07 +0200

    checking pre-commit hook

--- server/app/build.gradle.kts
+++ server/app/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.gradle.kotlin.dsl.implementation
 
 plugins {
-    java
-    id("org.springframework.boot") version "3.5.6"
-    id("io.spring.dependency-management") version "1.1.7"
+  java
+  id("org.springframework.boot") version "3.5.6"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.sokolowska"
@@ -12,17 +12,15 @@ version = "0.0.1-SNAPSHOT"
 
 description = "Application Entry Point"
 
-java {
-    toolchain { languageVersion = JavaLanguageVersion.of(21) }
-}
+java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
 repositories { mavenCentral() }
 
 dependencies {
-    implementation(project(":transaction"))
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation(project(":transaction"))
+  implementation("org.springframework.boot:spring-boot-starter")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
